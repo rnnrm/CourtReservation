07341e46aeeb008faef72a4975147b96779657bd
Christiaan Scholtz <christiaanscholtz@protonmail.com>
2026-07-08 18:35:24 +0200

    set next config turbopack path relative

--- courtbooking-server/next.config.ts
+++ courtbooking-server/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     turbopack: {
-        // relative to this config file
-        root: 'C:/myne/code/dotnet/v9/CourtBooking/courtbooking-server',
+        root: '.',
     },
     staticPageGenerationTimeout: 120,
     /* config options here */
