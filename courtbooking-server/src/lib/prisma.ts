c30feac9bb0f3c985b402d6c8680fd2079d87043
Christiaan Scholtz <christiaanscholtz@protonmail.com>
2026-07-08 21:15:44 +0200

    ai suggested fixes...

--- courtbooking-server/src/lib/prisma.ts
+++ courtbooking-server/src/lib/prisma.ts
@@ -1,5 +1,5 @@
 //import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
